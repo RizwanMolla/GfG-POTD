91475e5be5e02bdeb239a2d3960e025f5991fd84
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2025-02-27 21:52:12 +0530

    POTD 27-02-2025

---
+++ Get Min from Stack/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+    def pop(self):
+        if self.stack:
+            if self.stack[-1] == self.min_stack[-1]:
+                self.min_stack.pop()
+            self.stack.pop()
+
+    def peek(self):
+        return self.stack[-1] if self.stack else -1
+
+    def getMin(self):
+        return self.min_stack[-1] if self.min_stack else -1
