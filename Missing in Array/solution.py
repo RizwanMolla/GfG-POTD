1b33c8f397dc6bab0df2b25c825ad14c6a5f0519
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2025-04-21 20:56:04 +0530

    POTD 21-04-2025

---
+++ Missing in Array/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNum(self, arr):
+        # code here
+        n = len(arr) + 1
+        total_sum = n * (n + 1) // 2
+        actual_sum = sum(arr)
+        return total_sum - actual_sum
