e6cdc641a0ac112bd14ee05bcbc554e6059380be
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2026-04-19 12:36:52 +0530

    POTD 19-04-2026

---
+++ Check for Power/solution.py
@@ -0,0 +1,9 @@
+﻿import math
+
+class Solution:
+    def isPower(self, x, y):
+        if x == 1:
+            return y == 1
+        
+        res = math.log(y, x)
+        return abs(res - round(res)) < 1e-10
