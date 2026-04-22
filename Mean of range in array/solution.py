4961a7e21150630c1db1aebed86d85eef61f55bb
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2026-04-22 10:27:40 +0530

    POTD 22-04-2026

---
+++ Mean of range in array/solution.py
@@ -0,0 +1,23 @@
+﻿class Solution:
+    def findMean(self, arr, queries):
+        n = len(arr)
+        
+        # Prefix sum array
+        prefix = [0] * n
+        prefix[0] = arr[0]
+        
+        for i in range(1, n):
+            prefix[i] = prefix[i-1] + arr[i]
+        
+        result = []
+        
+        for l, r in queries:
+            if l == 0:
+                total = prefix[r]
+            else:
+                total = prefix[r] - prefix[l-1]
+            
+            length = r - l + 1
+            result.append(total // length)  # floor mean
+        
+        return result
