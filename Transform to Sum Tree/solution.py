2b2bee37ade7ffc2aa97de02e08a66afa5a3d6dc
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2026-05-23 21:50:46 +0530

    POTD 23-05-2026

---
+++ Transform to Sum Tree/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def toSumTree(self, root):
+        # code here
+        def solve(node):
+            if node is None:
+                return 0
+                 
+            left_tree_sum = solve(node.left)
+            right_tree_sum = solve(node.right)
+            
+            curr_node_val =  node.data
+            node.data = left_tree_sum + right_tree_sum
+            
+            # return total
+            return curr_node_val + node.data
+            
+        solve(root)
