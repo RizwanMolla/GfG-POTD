27f73eeeaa830731829e96557221e21434bd11cc
MD RIZWAN MOLLA <rizwanmolla711410@gmail.com>
2026-07-27 22:29:21 +0530

    POTD 27-07-2026

--- Complete Binary Tree Traversal with Array Input/solution.py
+++ Complete Binary Tree Traversal with Array Input/solution.py
@@ -1,1 +1,15 @@
-Construct a Full Binary Tree
+class Solution:
+    def levelSort(self, arr):
+        lth=len(arr)
+        ret=[]
+        q=[1]
+        while q:
+            ret.append(sorted([arr[ix-1] for ix in q]))
+            nq=[]
+            for cur in q:
+                if cur*2<=lth:
+                    nq.append(cur*2)
+                if cur*2+1<=lth:
+                    nq.append(cur*2+1)
+            q=nq
+        return ret
